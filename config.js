be25d700de65a738e1860810c2dc110a9f75e7cb
Jérôme Vasseur <jerome.vasseur@jhome.fr>
2018-07-13 16:17:45 +0200

    Add first version

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    extends: 'airbnb-base',
+    rules: {
+        'arrow-parens': ['error', 'always'],
+        curly: ['error', 'all'],
+        'import/prefer-default-export': ['warn'],
+        indent: ['error', 4, { SwitchCase: 1 }],
+        'max-len': ['warn', 120],
+        'no-multiple-empty-lines': ['error', { max: 1, maxBOF: 0, maxEOF: 0 }],
+        'no-plusplus': 'off',
+        'no-restricted-syntax': ['error', 'ForInStatement', 'LabeledStatement', 'WithStatement'],
+        'object-curly-newline': 'off',
+        'padding-line-between-statements': ['error', { blankLine: 'always', prev: '*', next: 'return' }],
+        yoda: ['error', 'always', { onlyEquality: true }]
+    }
+};
+
