1ed2da1f0adb41a8b4f3801c3867c7e20c581c68
Jérôme Vasseur <jerome.vasseur@thetribe.io>
2021-01-07 16:23:20 +0100

    Add eslint-config-typescript package

---
+++ packages/eslint-config-typescript/README.md
@@ -0,0 +1,18 @@
+# theTribe eslint config for TypeScript
+
+## Installation
+
+```sh
+yarn add --dev @thetribe/eslint-config-typescript @typescript-eslint/eslint-plugin eslint-plugin-import
+```
+
+## Configuration
+
+```json
+// .eslintrc.json
+
+{
+    "root": true,
+    "extends": "@thetribe/eslint-config-typescript"
+}
+```
---
+++ packages/eslint-config-typescript/index.js
@@ -0,0 +1,32 @@
+module.exports = {
+    extends: [
+        require.resolve('@thetribe/eslint-config'),
+        'plugin:import/typescript',
+    ],
+    parser: require.resolve('@typescript-eslint/parser'),
+    plugins: [
+        '@typescript-eslint',
+    ],
+    rules: {
+        // Replace base eslint rules with rules from typescript-eslint
+        '@typescript-eslint/no-shadow': ['error'],
+        '@typescript-eslint/no-unused-vars': ['error'],
+        'no-shadow': 'off',
+        'no-unused-vars': 'off',
+        // Add ts to allowed extensions
+        'import/extensions': ['error', 'ignorePackages', {
+            cjs: 'never',
+            js: 'never',
+            mjs: 'never',
+            ts: 'never',
+        }],
+    },
+    settings: {
+        'import/extensions': [
+            '.cjs',
+            '.js',
+            '.mjs',
+            '.ts',
+        ],
+    },
+};
